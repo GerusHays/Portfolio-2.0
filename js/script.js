fa687122e71fa7b364246c3c8a9664d658c0366b
Hays4Days <gerushays@gmail.com>
2023-02-23 08:41:41 -0700

    portfolio links added and some general cleanup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed(".typing",{
-    strings:["","Software Developer","Web Designer","Adaptive","Combat Veteran"],
+    strings:["","Software Developer","Web Designer","Problem Solver","Combat Veteran"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
